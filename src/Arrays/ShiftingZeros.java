94779c311606a06db76750437cd5240ce559dd55
im-faix <faexan92@gmail.com>
2025-03-19 17:12:11 +0530

    Create a new Project

---
+++ src/Arrays/ShiftingZeros.java
@@ -0,0 +1,10 @@
+package Arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ShiftingZeros {
+    public static void main(String[] args) {
+        
+            }
+        
+
+}
