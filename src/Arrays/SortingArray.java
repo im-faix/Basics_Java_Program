2d93dd288504ac66d3045d6c7c81eac5bd9a6eb6
im-faix <faexan92@gmail.com>
2025-03-23 14:41:27 +0530

    Sorting a Array in ascending or Descending order

--- src/Arrays/SortingArray.java
+++ src/Arrays/SortingArray.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 /*
  *  Input arr = [7,9,8,2,3,4]
- * Ouput arr = [2,3,4,7,8]
- *  
+ * Ouput arr = [2,3,4,7,8,9]
+ *  Output arr = [9,8,7,4,3,2]
  */
 public class SortingArray {
     public static void main(String[] args) {
@@ -24,10 +24,14 @@ public class SortingArray {
 
         System.out.println("\n After Sorting");
         System.out.println("=".repeat(20));
-        System.out.println(Arrays.toString(sortArray(arr)));
+        System.out.println("Sorting in Ascending Order");
+        System.out.println(Arrays.toString(sortArrayAscending(arr)));
+        System.out.println("=".repeat(20));
+        System.out.println("Sorting in Descending Order");
+        System.out.println(Arrays.toString(sortArrayDescending(arr)));
 
     }
-    public static int[] sortArray(int[] arr)
+    public static int[] sortArrayAscending(int[] arr)
     {
             if(arr.length==1)
                 return arr;
@@ -35,6 +39,23 @@ public class SortingArray {
             {
                 for (int j = 0; j < arr.length; j++) {
                     if(arr[i]<arr[j])
+                    {
+                        int temp = arr[j];
+                        arr[j]=arr[i];
+                        arr[i]=temp;
+                    }
+                }
+            }
+            return arr;
+    }
+    public static int[] sortArrayDescending(int[] arr)
+    {
+            if(arr.length==1)
+                return arr;
+            for(int i=0;i<arr.length;i++)
+            {
+                for (int j = 0; j < arr.length; j++) {
+                    if(arr[i]>arr[j])
                     {
                         int temp = arr[j];
                         arr[j]=arr[i];
