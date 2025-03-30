2356b1d9b7f625b0d030952d7e43f7cf00222ad5
im-faix <faexan92@gmail.com>
2025-03-30 12:36:34 +0530

    Reverse a each word in the string

--- src/Strings/ReverseEachWord.java
+++ src/Strings/ReverseEachWord.java
@@ -7,18 +7,27 @@ public class ReverseEachWord {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string ");
         String str = sc.nextLine();
+        System.out.println("Original String ");
         
+        System.out.println(str);
+        System.out.println("After Reverse Each Word In String");
+        System.out.println(reverseWords(str));
     }
     public static String reverseWords(String s)
     {
         String[] str = s.split(" ");
+        String words = "";
         for(int i=0;i<str.length;i++)
         {
             String word = str[i];
-            for(int j=0;j<word.length();j++)
+            String reverse = "";
+            for(int j=word.length()-1;j>=0;j--)
             {
-                
+                reverse += word.charAt(j);
             }
+            words += reverse +" ";
+
         }
+        return words.trim();
     }
 }
