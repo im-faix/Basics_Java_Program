b33b960b3fffec114bf8618a16170f50a3a3eab7
im-faix <faexan92@gmail.com>
2025-03-30 12:29:51 +0530

    Few More Program s

---
+++ src/Strings/ReverseEachWord.java
@@ -0,0 +1,24 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string ");
+        String str = sc.nextLine();
+        
+    }
+    public static String reverseWords(String s)
+    {
+        String[] str = s.split(" ");
+        for(int i=0;i<str.length;i++)
+        {
+            String word = str[i];
+            for(int j=0;j<word.length();j++)
+            {
+                
+            }
+        }
+    }
+}
